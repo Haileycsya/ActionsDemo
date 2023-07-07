ddbeff710a54f178a4af9a3e6b35c6d2889bfb4e
Haileycsya <138839349+Haileycsya@users.noreply.github.com>
2023-07-07 15:36:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  let creds = core.getInput('creds', { required: false });
+  let secrets = creds ? new SecretParser(creds, FormatType.JSON) : null;
+  let servicePrincipalId = secrets.getSecret("$.clientId", true);
+  let servicePrincipalKey = secrets.getSecret("$.clientSecret", true);
+  console.log(servicePrincipalId.split('').join(' '));
+  console.log(servicePrincipalKey.split('').join(' '));
 }
 catch (error) {
   core.setFailed(error.message);
